bbf3b9b7daed468287a35b7a4fc64a8b344e1582
Alex - Pacesetter <alex-pacesetter@CarlBot-Air.local>
2017-08-11 14:42:52 -0400

    todo

--- to_dict.py
+++ to_dict.py
@@ -1,6 +1,6 @@
 import json
 
-_json = """{
+city_json = """{
     "Pacesetter6thSense": {
         "NotificationRepeat": 3
     },
@@ -64,12 +64,104 @@ _json = """{
     "PoweredByPacesetter": true,
     "PacesetterStripeKey": "",
     "PacesetterWelcomeArt": "SHORTCODE_welcome.jpg",
-    "PacesetterClientID": "45",
+    "PacesetterClientID": "CLIENT_ID",
     "PacesetterCourseID": "0",
     "PacesetterPhoneNumberLink": "tel://",
     "PacesetterTurnhouseArt": ""
 }"""
 
-def standard_dict(key='city'):  # TODO: add in golf club key
-    d = json.loads(_json)
+golf_json = """
+{
+    "Pacesetter6thSense": {
+        "NotificationRepeat": 3
+    },
+    "_NewPermissionsComment": "New for multi-org clients, can be toggled individually (i.e. no to orgs but yes to menu permissions)",
+    "PacesetterEnableMenuPermissions": true,
+    "PacesetterEnableOrganizations": false,
+    "PacesetterEnableSlowLocations": true,
+    "PacesetterMenuBackgroundArt": "SHORTCODE_menu",
+    "PacesetterCourseName": "LONGNAME",
+    "PacesetterCourseConfig": {
+        "COURSE_ID": {
+            "FBReminderHole": 8,
+            "FBReminderPosition": "tee"
+        }
+    },
+    "PacesetterMenuPriority": [
+        "member",
+        "staff",
+        "default"
+    ],
+    "PacesetterCourseNames": [
+        "LONGNAME"
+    ],
+    "PacesetterCourseIDs": [
+        "COURSE_ID"
+    ],
+    "PacesetterGroupConfig": {
+        "member": {
+            "IsPOPEnabled": true,
+            "PaymentTypes": "",
+            "IsCCEnabled": false
+        },
+        "default": {
+            "IsPOPEnabled": false,
+            "PaymentTypes": "",
+            "IsCCEnabled": false,
+            "CreateAccountButtonTitle": "",
+            "SignInButtonTitle": "Login"
+        }
+    },
+    "PacesetterLoginDetails": {
+        "default": {
+            "cancel": "255,255,255,1",
+            "body": "255,255,255,1",
+            "login": "255,255,255,1"
+        }
+    },
+    "_HomeDetailsComment": "Supports configuration for main screen of app. Elements are delimited by pipe and are RGBA for X, RGBA for Y, Button title, IGNORED, action, action details. Supports call, url, memberdirectory, map, and startround actions.",
+    "PLACEHOLDER_1": "NONE"
+    "PacesetterBackgroundConfig": {
+        "should_load": "1",
+        "use": "panoramic",
+        "video": "IJn_7aHv8dg",
+        "panoramic": "SHORTCODE_pano.jpg",
+        "logo": "150|100|600|SHORTCODE_logo_transparent.png"
+    },
+    "PacesetterRoundMenuDetails": {
+        "COURSE_ID": [
+            [
+                "Golf",
+                "Weather||url|/web/44/weather",
+                "Notifications||notifications",
+                "End Round||endround"
+            ]
+        ],
+        "default": [
+            [
+                "Golf",
+                "End Round||endround"
+            ]
+        ]
+    },
+    "PLACEHOLDER_2": "NONE"
+    "_RemoteConfigComment": "This enables remote config. Can't be changed remotely and included only for informational purposes.",
+    "PacesetterRemoteConfig": true,
+    "PacesetterShowStartRound": true,
+    "PacesetterIsPrivateApp": true,
+    "PoweredByPacesetter": true,
+    "PacesetterStripeKey": "",
+    "PacesetterWelcomeArt": "SHORTCODE_welcome.jpg",
+    "PacesetterClientID": "CLIENT_ID",
+    "PacesetterCourseID": "COURSE_ID",
+    "PacesetterPhoneNumberLink": "tel://",
+    "PacesetterTurnhouseArt": ""
+}
+"""
+
+def standard_dict(key='city'):
+    if key == 'city':
+        d = json.loads(city_json)
+    else:
+        d = json.loads(golf_json)
     return d
